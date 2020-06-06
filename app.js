a0b52d23e4e5f5d70abf1521b92e5ed488dacb00
Vitor Leonel <falecom@vitorleonel.com>
2020-06-06 19:46:04 -0300

    manage whasapp session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 async function Bot() {
   const browser = await puppeteer.launch({ headless: false });
@@ -7,9 +8,52 @@ async function Bot() {
 
   await page.setViewport({ width: 0, height: 0 });
   await page.goto("https://web.whatsapp.com", { waitUntil: "networkidle0" });
-  await page.waitFor(5000);
+
+  // Gerenciando sessão do WhatsApp
+  try {
+    const sessionContent = fs.readFileSync("./session.txt");
+    const sessionObject = JSON.parse(sessionContent);
+
+    await page.evaluate((sessionObject) => {
+      const keys = Object.keys(sessionObject);
+      let keysLength = keys.length;
+
+      while (keysLength--) {
+        let key = keys[keysLength];
+
+        window.localStorage.setItem(key, sessionObject[key]);
+      }
+    }, sessionObject);
+
+    await page.reload({ waitUntil: "networkidle0" });
+    await page.waitForSelector("div.app-wrapper-web .two", { timeout: 30000 });
+  } catch (error) {
+    await page.waitForSelector("div.app-wrapper-web .two", { timeout: 30000 });
+
+    const sessionObject = await page.evaluate(() => {
+      let response = {};
+
+      const keys = Object.keys(window.localStorage);
+      let keysLength = keys.length;
+
+      while (keysLength--) {
+        let key = keys[keysLength];
+
+        if (key.includes("==")) {
+          continue;
+        }
+
+        response[key] = window.localStorage.getItem(key);
+      }
+
+      return response;
+    });
+
+    fs.writeFileSync("./session.txt", JSON.stringify(sessionObject), "UTF-8");
+  }
 
   // Troque "eu" pelo nome do contato exibido na sua lista de conversas
+  await page.waitFor(2000);
   await page.click('._210SC span[title="Eu"]');
   await page.waitFor(1000);
 
@@ -25,7 +69,7 @@ async function Bot() {
   await page.waitFor(2000);
 
   await page.click('span[data-icon="send"]');
-  await page.waitFor(1000);
+  await page.waitFor(5000);
 
   await browser.close();
 }
